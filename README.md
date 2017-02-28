d1843ff6a7c9451936c30e4f121851ee74a386e8
Paul-Emmanuel Vandenburie <paulvand@cisco.com>
2017-02-28 23:28:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Quick-Rally bot
+===============
+
+Spark Bot for quick CA Agile Rally access for **SPVSS Infinite Home project**.
+
+It is based on the ["flint-bot"](https://github.com/flint-bot/flint) framework.
+
+
+## References
+
+[Spark API](https://spark.laravel.com/docs/3.0/api)
---
+++ app.js
@@ -0,0 +1,84 @@
+var Flint = require('node-flint');
+var webhook = require('node-flint/webhook');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+// flint options
+var config = {
+  webhookUrl: 'http://myserver.com/flint',
+  token: process.env.SPARK_TOKEN,
+  port: 3001
+};
+
+//init flint
+var flint = new Flint(config);
+flint.start();
+
+// say hello
+flint.hears('/hello', function(bot, trigger) {
+  bot.say('Hello %s!', trigger.personDisplayName);
+});
+
+// define express path for incoming webhooks
+app.post('/flint', webhook(flint));
+
+// start express server
+var server = app.listen(config.port, function() {
+  flint.debug('Flint listening on port %s', config.port);
+});
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  flint.debug('Listening on ' + bind);
+}
+
+
+// gracefully shutdown (crtl-c)
+process.on('SIGINT', function() {
+  flint.debug('stopping...');
+  server.close();
+  flint.stop().then(function() {
+    process.exit();
+  });
+});
