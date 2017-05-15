d1b3cb3e42ace34f57b76cc96ab8c39286a8c3cf
Paul-Emmanuel Vandenburie <paulvand@cisco.com>
2017-05-15 22:27:37 +0200

    Added tests for Mailer.

---
+++ test/test_mailer_trains.js
@@ -0,0 +1,17 @@
+var mailer = require('../lib/takeatrain/lib/util').mailer;
+
+function print_result(result) {
+  result.forEach( function(row) {
+    console.log(row);
+  });
+}
+
+var trains = ["Purple Train", "Brown Train", "Saffron Train", "Green Train", "Navy Train", "Grey Train", "Blue Train", "Gold Train", "Orange Train", "Pink Train"];
+
+trains.forEach( function(trainName) {
+  mailer.searchTrain(trainName, function(response) {
+    console.log('Found for '+trainName+': ');
+    print_result(response);
+    console.log('------------------------------------------');
+  });
+});
